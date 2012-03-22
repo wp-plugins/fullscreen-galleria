d3c55896837d50d87fc64ce3ce215f149295b5e4
pdamsten <pdamsten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-22 12:01:04 +0000

    Add map for images that have gps coordinates
    git-svn-id: https://plugins.svn.wordpress.org/fullscreen-galleria/tags/0.5@522127 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ galleria-fs.js
@@ -0,0 +1,226 @@
+/******************************************************************************
+ Galleria Fullscreen Theme
+ Copyright (c) 2012, Petri Damstén
+ Licensed under the MIT license.
+******************************************************************************/
+
+(function($) {
+
+var fsg_last_post_id = "";
+
+$(document).ready(function() { // DOM ready
+  $("[data-imgid]", this).each(function() {
+    $(this).click(show_galleria);
+  });
+  if($(".galleria-photobox").length != 0) {
+    randomize_photos();
+  }
+});
+
+set_keyboard = function(event) {
+  var galleria = $("#galleria").data('galleria');
+  galleria.attachKeyboard({
+    escape: function() {
+      if ($('#galleria-map').is(":visible")) {
+        $('.galleria-map-close').click();
+      } else {
+        $('.galleria-close').click();
+      }
+    }
+  });
+}
+
+show_galleria = function(event) {
+  event.preventDefault();
+  var elem = $("#galleria");
+  var close = $("#close-galleria");
+  elem.toggle();
+  close.toggle();
+  var postid = $(this).attr("data-postid");
+  var imgid = $(this).attr("data-imgid");
+  if (postid != fsg_last_post_id) {
+    if (elem.data('galleria')) {
+      // Set new data
+      // Bit of a hack, but load does not have show param and show function
+      // works purely after load
+      elem.data('galleria')._options.show = parseInt(imgid);
+      elem.data('galleria').load(fsg_json[postid]);
+      elem.data('galleria').enterFullscreen();
+      set_keyboard();
+    } else {
+      // Init galleria
+      elem.galleria({
+        dataSource: fsg_json[postid],
+        show: imgid,
+        showCounter: false,
+        fullscreenDoubleTap: false,
+        imageCrop: false,
+        fullscreenCrop: false,
+        maxScaleRatio: 1.0,
+        idleTime: 2000,
+        extend: function() {
+          this.enterFullscreen();
+          set_keyboard();
+        }
+      });
+    }
+    fsg_last_post_id = postid;
+  } else {
+    elem.data('galleria').show(imgid);
+    elem.data('galleria').enterFullscreen();
+    set_keyboard();
+  }
+}
+
+open_map = function(lat, long)
+{
+  $('#galleria-map').show();
+  if (typeof open_map.map == 'undefined') {
+    open_map.proj = new OpenLayers.Projection('EPSG:4326');
+    OpenLayers.ImgPath = fullscreen_galleria_url;
+    open_map.map = new OpenLayers.Map('galleria-map', {
+        controls:[
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.PanZoomBar(),
+            new OpenLayers.Control.Attribution()]
+    });
+    open_map.map.addLayer(new OpenLayers.Layer.OSM());
+  }
+
+  var lonLat = new OpenLayers.LonLat(long, lat).transform(
+      open_map.proj, open_map.map.getProjectionObject());
+  open_map.map.setCenter(lonLat, 16);
+
+  if (typeof open_map.marker == 'undefined') {
+    var markers = new OpenLayers.Layer.Markers('Markers');
+    open_map.map.addLayer(markers);
+    var size = new OpenLayers.Size(35, 52);
+    var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
+    var icon = new OpenLayers.Icon(fullscreen_galleria_url + 'marker.png', size, offset);
+    open_map.marker = new OpenLayers.Marker(lonLat, icon);
+    markers.addMarker(open_map.marker);
+  } else {
+    var px = open_map.map.getLayerPxFromLonLat(lonLat);
+    open_map.marker.moveTo(px);
+  }
+}
+
+randomize_photos = function()
+{
+  $(".galleria-photobox").each(function(index) {
+    var ID = 'fsg_photobox_' + index;
+    var BORDER = fsg_photobox[ID]['border'];
+    var COLS = fsg_photobox[ID]['cols'];
+    var ROWS = fsg_photobox[ID]['rows'];
+    var BOX = $(this).width() / COLS + 1;
+    $(this).width($(this).parent().width() + 2 * BORDER);
+    $(this).height(($(this).parent().width() / COLS * ROWS) + 2 * BORDER);
+    $(this).css('top', -BORDER);
+    $(this).css('left', -BORDER);
+    $(this).html('');
+    //console.log(index, ID, BORDER, 'x', COLS, ROWS, BOX);
+
+    // init array
+    var array = new Array(COLS);
+    for (var i = 0; i < COLS; i++) {
+      array[i] = new Array(ROWS);
+      for (var j = 0; j < ROWS; j++) {
+        array[i][j] = 0;
+      }
+    }
+    var x = 0;
+    var y = 0;
+    var d = 1;
+    while (1) {
+      // next free cell
+      stop = false;
+      while (array[x][y] != 0) {
+        ++x;
+        if (x >= COLS) {
+          x = 0;
+          ++y;
+          if (y >= ROWS) {
+            stop = true;
+            break;
+          }
+        }
+      }
+      if (stop) {
+        break;
+      }
+      // find max size
+      var mx = 0;
+      while ((x + mx) < COLS && array[x + mx][y] == 0) {
+        ++mx;
+      }
+      var my = 0;
+      while ((y + my) < ROWS && array[x][y + my] == 0) {
+        ++my;
+      }
+      // mark array
+      var max = Math.min(mx, my);
+      var box = Math.floor(Math.random() * max) + 1;
+      for (var i = 0; i < box; i++) {
+        for (var j = 0; j < box; j++) {
+          array[x + i][y + j] = d;
+        }
+      }
+      // Get next random photo
+      var all = true;
+      var photo = Math.floor(Math.random() * fsg_json[ID].length);
+      for (i = 0; i < fsg_json[ID].length; ++i) {
+        if (fsg_json[ID][photo]['used'] != true) {
+          fsg_json[ID][photo]['used'] = true;
+          all = false;
+          break;
+        }
+        ++photo;
+        if (photo >= fsg_json[ID].length) {
+          photo = 0;
+        }
+      }
+      if (all) {
+        for (i = 0; i < fsg_json[ID].length; ++i) {
+          fsg_json[ID][i]['used'] = (i == photo);
+        }
+      }
+      // Add photo div
+      var size = (box * BOX - 2 * BORDER);
+      var img = fsg_json[ID][photo]['image'];
+      var w = fsg_json[ID][photo]['full'][1];
+      var h = fsg_json[ID][photo]['full'][2];
+      //console.log(size, y, x, y * BOX, x * BOX);
+      var $div = $('<div style="width: ' + size + 'px; height: ' + size + 'px; top: ' + y * BOX +
+                  'px; left: ' + x * BOX + 'px; margin: ' + BORDER + 'px;">');
+      var $a = $('<a data-postid="' + ID + '" data-imgid="' + photo + '" href="' + img + '">');
+      $($a).click(show_galleria);
+      // - Find best img
+      var a = ["thumbnail", "medium", "large", "full"];
+      for (var s in a) {
+        min = Math.min(fsg_json[ID][photo][a[s]][1],
+                      fsg_json[ID][photo][a[s]][2]);
+        if (min > size) {
+          img = fsg_json[ID][photo][a[s]][0];
+          w = fsg_json[ID][photo][a[s]][1];
+          h = fsg_json[ID][photo][a[s]][2];
+          break;
+        }
+      }
+      var min = Math.min(w, h);
+      var m = size / min;
+      w = w * m;
+      h = h * m;
+      var imgx = -Math.floor((w - size) / 2.0);
+      var imgy = -Math.floor((h - size) / 2.0);
+      var $img = $('<img style="left: ' + imgx + 'px; top: ' + imgy + 'px;" width="' + w +
+                  '" height="' + h + '" src="' + img + '">');
+      $a.append($img);
+      $div.append($a);
+      $(this).append($div);
+      ++d;
+    }
+  });
+  //$(window).resize();
+}
+
+}(jQuery)); 
---
+++ galleria-fs.php
@@ -0,0 +1,407 @@
+<?php
+/******************************************************************************
+
+Plugin Name: Galleria Fullscreen
+Plugin URI: http://torturedmind.org/
+Description: Fullscreen gallery for Wordpress
+Version: 0.5
+Author: Petri Damstén
+Author URI: http://torturedmind.org/
+License: MIT
+
+******************************************************************************/
+
+$ver = '0.5';
+
+class GFSPlugin {
+  protected $photobox = "fsg_photobox = {\n";
+  protected $json = "fsg_json = {\n";
+  protected $gps = FALSE;
+  protected $photoboxid = 0;
+  protected $groupid = 0;
+  protected $used = Array();
+
+  function startswith(&$str, &$starts)
+  {
+    return (strncmp($str, $starts, strlen($starts)) == 0);
+  }
+
+  function tagarg(&$tag, $arg)
+  {
+    $i = stripos($tag, $arg);
+    if ($i != FALSE) {
+      $len = strlen($tag);
+      while ($i < $len && $tag[$i] != "'" && $tag[$i] != '"') {
+        ++$i;
+      }
+      ++$i;
+      $j = stripos($tag, $tag[$i - 1], $i);
+      if ($j != FALSE) {
+        return substr($tag, $i, $j - $i);
+      }
+    }
+    return '';
+  }
+
+  function href($str)
+  {
+    $href = $this->tagarg($str, 'href');
+    if (WP_DEBUG) {
+      // Make localhost copy work in DEBUG mode
+      $bloginfo = get_bloginfo('url');
+      if (strrpos($bloginfo, "localhost") !== FALSE) {
+        $href = str_replace(array(".org", ".net", ".com"), ".localhost", $href);
+      }
+    }
+    return $href;
+  }
+
+  function links($content)
+  {
+    $links = array();
+    $re = "/\<a[^\>]+\>/mi";
+    preg_match_all($re, $content, $links);
+    $links = $links[0];
+    return $links;
+  }
+  
+  function gps_to_float($value)
+  {
+    $a = explode('/', $value);
+    if (count($a) < 1) {
+      return 0.0;
+    }
+    if (count($a) < 2) {
+      return floatval($a[0]);
+    }
+    return floatval($a[0]) / floatval($a[1]);
+  }
+
+  function gps_to_degrees($value)
+  {
+    if (count($value) > 0) {
+      $d = $this->gps_to_float($value[0]);
+    }
+    if (count($value) > 1) {
+      $m = $this->gps_to_float($value[1]);
+    }
+    if (count($value) > 2) {
+      $s = $this->gps_to_float($value[2]);
+    }
+    return $d + ($m / 60.0) + ($s / 3600.0);
+  }
+
+  public function __construct()
+  {
+    // run after gallery processed
+    add_filter('the_content', array(&$this, 'content'), 99);
+    add_action('wp_enqueue_scripts', array(&$this, 'enqueue_scripts'));
+    add_action('wp_footer', array(&$this, 'footer'));
+    add_filter('attachment_fields_to_edit', array(&$this, 'fields_to_edit'), 10, 2);
+    add_filter('attachment_fields_to_save', array(&$this, 'fields_to_save'), 10, 2);
+    add_filter('wp_read_image_metadata', array(&$this, 'add_additional_metadata'), '', 3);
+    add_shortcode('fsg_photobox', array(&$this, 'photobox_shortcode'));
+    add_shortcode('fsg_link', array(&$this, 'link_shortcode'));
+  }
+
+  function add_additional_metadata($meta, $file, $sourceImageType)
+  {
+    error_log($file);
+    $exif = @exif_read_data($file);
+    error_log('1');
+    if (!empty($exif['GPSLatitude'])) {
+      $lat = $this->gps_to_degrees($exif['GPSLatitude']);
+    }
+    error_log('2');
+    if (!empty($exif['GPSLongitude'])) {
+      $long = $this->gps_to_degrees($exif['GPSLongitude']);
+    }
+    if (!empty($exif['GPSLatitudeRef'])) {
+      if ($meta['image_meta']['latitude_ref'] == 'S') {
+        $lat *= -1;
+      }
+    }
+    error_log('3');
+    if (!empty($exif['GPSLongitudeRef'])) {
+      if ($meta['image_meta']['longitude_ref'] == 'W') {
+        $long *= -1;
+      }
+    }
+    error_log('4');
+    if (isset($long)) {
+      $meta['longitude'] = $long;
+    }
+    if (isset($lat)) {
+      $meta['latitude'] = $lat;
+    }
+    error_log('5');
+    return $meta;
+  }
+
+  function photobox_shortcode($attr, $content = null)
+  {
+    global $post;
+
+    extract(shortcode_atts(array(
+      'rows'       => 2,
+      'cols'       => 3,
+      'border'     => 2,
+      'include'    => '',
+    ), $attr));
+
+    if (!empty($include)) {
+      $photos = &get_posts(array('post_type' => 'attachment',
+          'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID',
+          'include' => $include));
+    } else {
+      $photos = &get_children(array('post_parent' => $post->ID, 'post_status' => 'inherit',
+          'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC',
+          'orderby' => 'menu_order ID'));
+    }
+    $images = array();
+    foreach ($photos as $key => $val) {
+      $images[$this->href(wp_get_attachment_link($val->ID, 'full'))] =
+          array('post_id' => $val->ID, 'id' => 0, 'data' => $val);
+    }
+    $id = 'fsg_photobox_'.$this->photoboxid;
+    $this->photobox .= $id.": {rows: ".$rows.", cols: ".$cols.", border: ".$border."},\n";
+    $this->append_json($id, $images, true);
+    ++$this->photoboxid;
+    return "<div id='".$id."' class='galleria-photobox'></div>";
+  }
+
+  function link_shortcode($attr, $content = null)
+  {
+    global $post;
+
+    extract(shortcode_atts(array(
+      'include'    => '',
+      'class'      => '',
+    ), $attr));
+
+    if (!empty($include)) {
+      $photos = &get_posts(array('post_type' => 'attachment',
+          'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID',
+          'include' => $include));
+      $id = "fsg_group_".$this->groupid;
+      ++$this->groupid;
+    } else {
+      $photos = &get_children(array('post_parent' => $post->ID, 'post_status' => 'inherit',
+          'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC',
+          'orderby' => 'menu_order ID'));
+      $id = "fsg_post_".$post->ID;
+    }
+    $images = array();
+    foreach ($photos as $key => $val) {
+      $images[$this->href(wp_get_attachment_link($val->ID, 'full'))] =
+          array('post_id' => $val->ID, 'id' => 0, 'data' => $val);
+    }
+    if (!empty($include)) {
+      $this->append_json($id, $images);
+      $this->used = array_merge($this->used, wp_parse_id_list($include));
+    }
+    reset($images);
+    $first = key($images);
+    if (!empty($class)) {
+      $class = " class='".$class."'";
+    }
+    return "<a data-postid='".$id."' data-imgid='0' href='".$first."'".$class.">".$content."</a>";
+  }
+
+  function fields_to_edit($form_fields, $post)
+  {
+    $meta = wp_get_attachment_metadata($post->ID);
+    $form_fields["camera-info"] = array(
+        "label" => __("Camera Info"),
+        "input" => "text",
+        "value" => empty($meta['image_meta']['info']) ? '' : $meta['image_meta']['info'],
+        "helps" => __('Camera info for Fullscreen Galleria plugin.')
+    );
+    $form_fields["custom-link"] = array(
+        "label" => __("Custom Link"),
+        "input" => "text",
+        "value" => empty($meta['image_meta']['link']) ? '' : $meta['image_meta']['link'],
+        "helps" => __('Custom link for Fullscreen Galleria plugin.')
+    );
+    $form_fields["latitude"] = array(
+        "label" => __("Latitude"),
+        "input" => "text",
+        "value" => empty($meta['image_meta']['latitude']) ? '' : $meta['image_meta']['latitude'],
+        "helps" => __('Latitude for Fullscreen Galleria plugin.')
+    );
+    $form_fields["longitude"] = array(
+        "label" => __("Longitude"),
+        "input" => "text",
+        "value" => empty($meta['image_meta']['longitude']) ? '' : $meta['image_meta']['longitude'],
+        "helps" => __('Longitude for Fullscreen Galleria plugin.')
+    );
+    return $form_fields;
+  }
+
+  function fields_to_save($post, $attachment)
+  {
+    $meta = wp_get_attachment_metadata($post['ID']);
+    $modified = false;
+    if (isset($attachment['camera-info'])) {
+      $meta['image_meta']['info'] = $attachment['camera-info'];
+      $modified = true;
+    }
+    if (isset($attachment['custom-link'])) {
+      $meta['image_meta']['link'] = $attachment['custom-link'];
+      $modified = true;
+    }
+    if (isset($attachment['latitude'])) {
+      $meta['image_meta']['latitude'] = $attachment['latitude'];
+      $modified = true;
+    }
+    if (isset($attachment['longitude'])) {
+      $meta['image_meta']['longitude'] = $attachment['longitude'];
+      $modified = true;
+    }
+    if ($modified) {
+      wp_update_attachment_metadata($post['ID'], $meta);
+    }
+    return $post;
+  }
+
+  function enqueue_scripts()
+  {
+    wp_enqueue_script('galleria', plugins_url('galleria-1.2.6.min.js', __FILE__), array('jquery'), '1.2.6', true);
+    //wp_enqueue_script('galleria', plugins_url('galleria.js', __FILE__), array('jquery'), '1.2.6', true);
+    wp_enqueue_script('galleria-fs', plugins_url('galleria-fs.js', __FILE__), array('galleria'), $ver, true);
+    // register here and print conditionally in footer
+    wp_register_script('open-layers', plugins_url('OpenLayers.js', __FILE__), array('galleria-fs'), '2.11', true);
+    wp_register_style('galleria-fs', plugins_url('galleria-fs.css', __FILE__), array(), $ver);
+    wp_enqueue_style('galleria-fs');
+  }
+
+  function footer()
+  {
+    // We call wp_print_scripts here also when gps is false so scripts get printed before
+    // json/galleria loading code
+    wp_print_scripts(($this->gps) ? 'open-layers' : '');
+    if (!empty($this->json)) {
+      $this->json .= "};";
+      $this->photobox .= "};\n";
+      $theme = plugins_url('galleria-fs-theme.js', __FILE__);
+      $url = "fullscreen_galleria_url='".plugin_dir_url(__FILE__)."';\n";
+      echo "<div id=\"galleria\"></div><script>Galleria.loadTheme(\"".$theme."\");\n".
+           $url.$this->photobox.$this->json."</script>";
+    }
+  }
+
+  function imginfo(&$meta)
+  {
+    // TODO Generate info from exif if there is a way to read lens id from makernote
+    if (!empty($meta['image_meta']['info'])) {
+      return $meta['image_meta']['info'];
+    }
+    return '';
+  }
+
+  function append_json($id, &$images, $extra = false)
+  {
+    // Write json data for galleria
+    $i = 0;
+    $this->json .= $id.": [\n";
+    foreach ($images as $key => $val) {
+      if (!in_array($val['post_id'], $this->used)) {
+        $meta = wp_get_attachment_metadata($val['post_id']);
+        $thumb = wp_get_attachment_image_src($val['post_id'], 'thumbnail');
+        $thumb = $thumb[0];
+        $title = str_replace("'", "\\'", $val['data']->post_title);
+        //var_dump($val['data']);
+        $description = $val['data']->post_content;
+        if (!empty($description)) {
+          $description = str_replace("'", "\\'", $description);
+          $description = str_replace("\n", "<br/>", $description);
+          $description = str_replace("\r", "", $description);
+          $description = "<p class=\"galleria-info-description\">".$description."</p>";
+        }
+        if (!empty($meta['image_meta']['link'])) {
+          $link = $meta['image_meta']['link'];
+          if (strpos($link, 'flickr.com') != FALSE) {
+            $t = 'Show in Flickr';
+            $c = 'galleria-link-flickr';
+          } else {
+            $t = $link;
+            $c = 'galleria-link';
+          }
+          $link = "<div class=\"galleria-layeritem\">".
+                      "<a target=\"_blank\" title=\"".$t."\" href=\"".$link."\">".
+                      "<div class=\"".$c."\"></div></a>".
+                  "</div>";
+        } else {
+          $link = '';
+        }
+        if (!empty($meta['image_meta']['longitude'])) {
+          $c = $meta['image_meta']['latitude'].",".$meta['image_meta']['longitude'];
+          $map = "<div class=\"galleria-layeritem\">".
+                      "<a title=\"Open Map\" href=\"#\" onclick=\"open_map(".$c.");\">".
+                      "<div class=\"galleria-link-map\"></div></a>".
+                  "</div>";
+          $this->gps = TRUE;
+        } else {
+          $map = '';
+        }
+        $info = (empty($meta['image_meta']['info'])) ? '' :
+                "<p class=\"galleria-info-camera\">".$meta['image_meta']['info']."</p>";
+        $this->json .= "{image: '".$key.
+                      "', thumb: '".$thumb.
+                      "', layer: '<div class=\"galleria-infolayer\">".
+                          "<div class=\"galleria-layeritem\">".
+                              "<h1>".$title."</h1>".$description.$info.
+                          "</div>".$link.$map."</div>'";
+        if ($extra) {
+          foreach (array("thumbnail", "medium", "large", "full") as $size) {
+            $img = wp_get_attachment_image_src($val['post_id'], $size);
+            $this->json .= ", ".$size.": ['".$img[0]."', ".$img[1].", ".$img[2]."]";
+          }
+        }
+        $this->json .= "},\n";
+        $images[$key]['id'] = $i;
+        ++$i;
+      }
+    }
+    $this->json .= "],\n";
+  }
+
+  // Handle gallery
+  function content($content)
+  {
+    global $post;
+    // Get children (images) of the post
+    $children = &get_children(array('post_parent' => $post->ID, 'post_status' => 'inherit',
+        'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC',
+        'orderby' => 'menu_order ID'));
+    if (empty($children)) {
+      return $content;
+    }
+    $images = array();
+    foreach ($children as $key => $val) {
+      $images[$this->href(wp_get_attachment_link($key, 'full'))] =
+          array('post_id' => $key, 'id' => 0, 'data' => $val);
+    }
+
+    // Get possible image groups
+    $links = $this->links($content);
+    $this->append_json('fsg_post_'.$post->ID, $images);
+
+    // Add needed data to links
+    foreach ($links as $link) {
+      if (strpos($link, 'data-postid') === false) { // test if link already has the data
+        $href = $this->href($link);
+        if (array_key_exists($href, $images)) {
+          $tmp = str_replace('<a ', '<a data-postid="fsg_post_'.$post->ID.
+                            '" data-imgid="'.$images[$href]['id'].'" ', $link);
+          $content = str_replace($link, $tmp, $content);
+        }
+      }
+    }
+    return $content;
+  }
+}
+
+$gfsplugin = new GFSPlugin();
+
+?>
